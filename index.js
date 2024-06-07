fafac842609fe81456d4dea12e9372a4f189d323
Alishba-F <alishbafahim50@gmail.com>
2024-06-07 09:14:20 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+
+// Left container elements
+dragElement(document.querySelector("#star1"));
+dragElement(document.querySelector("#star2")); 
+dragElement(document.querySelector("#star3"));
+dragElement(document.querySelector("#star4"));
+dragElement(document.querySelector("#star5")); 
+dragElement(document.querySelector("#star6"));
+
+// Right container elements
+dragElement(document.querySelector("#star7"));
+dragElement(document.querySelector("#star8"));
+dragElement(document.querySelector("#star9"));
+dragElement(document.querySelector("#star10"));
+dragElement(document.querySelector("#star11"));
+dragElement(document.querySelector("#star12"));
+
+
+function dragElement(WindowElement) {
+	let pos1 = 0,
+		pos2 = 0,
+		pos3 = 0,
+		pos4 = 0;
+	WindowElement.onpointerdown = pointerDrag;
+
+	function pointerDrag(e) {
+		e.preventDefault();
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+			document.onpointermove = elementDrag;
+			document.onpointerup = stopElementDrag;
+	}
+
+	function elementDrag(e) {
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+			WindowElement.style.top = WindowElement.offsetTop - pos2 + "px";
+			WindowElement.style.left = WindowElement.offsetLeft - pos1 + "px";
+	}
+
+	function stopElementDrag() {
+		document.onpointerup = null;
+		document.onpointermove = null;
+	}
+}
+
+
+// Reference to the code and tutorials for drag 
+// https://codepen.io/theslladev/pen/yLMoZmd
+// https://codepen.io/natacoops/post/a-drag-and-drop-succulent-garden-project-solution
+//https://www.youtube.com/watch?v=ymDjvycjgUM
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// dragElement(document.querySelector("#plant1"));
+// dragElement(document.querySelector("#plant2"));
+// dragElement(document.querySelector("#plant3"));
+// dragElement(document.querySelector("#plant4"));
+// dragElement(document.querySelector("#plant5"));
+// dragElement(document.querySelector("#plant6"));
+// dragElement(document.querySelector("#plant7"));
